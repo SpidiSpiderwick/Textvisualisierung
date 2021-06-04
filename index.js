cc5327d249b8df97b04e925df4614bf24db22624
spidispiderwick <chrissi.penndorf@gmail.com>
2021-06-04 11:05:50 +0200

    a little map

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ function example_function() {
 
     return;
 }
+
+function test() {
+    var mymap = L.map('mapid').setView([46.54, 2.44], 6);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3BpZGVyd2ljayIsImEiOiJja3BndGhsem8ya2l3Mm5ubG9qdmg1Y2I0In0.VLvrurO3hpyg39BlqImU8w', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'your.mapbox.access.token'
+    }).addTo(mymap);
+
+    mymap.on('click', onMapClick);
+}
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
