63937e361695a1fda1fdc1914ca747fa18fcdd2d
spidispiderwick <chrissi.penndorf@gmail.com>
2021-06-07 10:46:41 +0200

    Added points to map

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /**
  * example function
  */
-function test() {
-    var mymap = L.map('mapid').setView([46.54, 2.44], 6);
 
+var mymap;
+
+function test() {
+    mymap = L.map('mapid').setView([46.54, 2.44], 6);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3BpZGVyd2ljayIsImEiOiJja3BndGhsem8ya2l3Mm5ubG9qdmg1Y2I0In0.VLvrurO3hpyg39BlqImU8w', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
@@ -12,14 +14,6 @@ function test() {
         zoomOffset: -1,
         accessToken: 'your.mapbox.access.token'
     }).addTo(mymap);
-
-    L.circle([48.8, 2.3], 500, {
-        color: 'green',
-        fillColor: 'green',
-        fillOpacity: 0.5
-    }).addTo(mymap).bindPopup("I am a circle.");
-
-    var popup = L.popup();
 
     mymap.on('click', onMapClick);
 
@@ -35,7 +29,8 @@ function loadFile(file){
     let reader = new FileReader();
     reader.readAsBinaryString(file); //liest die Datei als String ein
     reader.onloadend = function(event){ //gibt an was passiert, wenn die Datei fertig geladen wurde
-        drawPoints(event.target.result);
+        data = JSON.parse(reader.result)
+        drawPoints(data);
     }
 }
 
@@ -44,7 +39,16 @@ function loadFile(file){
  * @param data
  */
 function drawPoints(data){
-    console.log(data)
+
+    for(var i = 0; i < data.length; i++) {
+        var obj = data[i];
+
+        L.circle([obj.lat, obj.long], 25, {
+            color: 'green',
+            fillColor: 'green',
+            fillOpacity: 0.5
+        }).addTo(mymap);
+    }
 }
 
 
